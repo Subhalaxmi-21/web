371aae99d6ba65358e04f1a0306b24c993584ddc
Subhalaxmi Das <subhalaxmi.das@somaiya.edu>
2022-05-10 12:06:39 +0530

    done

---
+++ AJAX/Form/process.php
@@ -0,0 +1,28 @@
+<?php
+
+$errors = [];
+$data = [];
+
+if (empty($_POST['name'])) {
+    $errors['name'] = 'Name is required.';
+}
+
+if (empty($_POST['email'])) {
+    $errors['email'] = 'Email is required.';
+}
+
+if (empty($_POST['superheroAlias'])) {
+    $errors['superheroAlias'] = 'Superhero alias is required.';
+}
+
+if (!empty($errors)) {
+    echo "MUsic";
+    $data['success'] = false;
+    $data['errors'] = $errors;
+} else {
+    $data['success'] = true;
+    $data['message'] = 'Success!';
+}
+
+echo json_encode($data);
+
---
+++ AJAX/Validation/final.php
@@ -0,0 +1,5 @@
+<h2>Here is the information submitted by you:</h2>
+<p>UserName: <b><?php echo $_POST["username"] ?? ''; ?></b></p>
+<p>Password: <b><?php echo $_POST["password"] ?? ''; ?></b></p>
+<p>Email: <b><?php echo $_POST["email"] ?? ''; ?></b></p>
+<p>Email: <b><?php echo $_POST["website"] ?? ''; ?></b></p>
